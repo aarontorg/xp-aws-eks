1fcf1a77b62be4b1621b240928c37da0a7a0a6ff
EC2 Default User <ec2-user@ip-192-168-63-112.us-gov-west-1.compute.internal>
2024-06-27 17:31:02 +0000

    changes

--- scripts/cluster-cfg.sh
+++ scripts/cluster-cfg.sh
@@ -1,18 +1,22 @@
-#! /bin/zsh
+#! /bin/bash
+
+# Simple script to update kubeconfig for all eks clusters
 
 # shellcheck disable=SC2207
 xk8s=( $(kubectl get xk8s | grep -v NAME | awk '{print $1}') )
 declare -a xk8s
 for x in "${xk8s[@]}" ;
 do
-  print "xk8: $x"
+  echo "xk8: $x"
   xk8sUid=$(kubectl get xk8s $x -o jsonpath='{.metadata.uid}');
-  print "xk8Uid: $xk8sUid"
+  echo "xk8Uid: $xk8sUid"
   clusterName=$(kubectl get xk8s $x -o jsonpath='{.spec.id}')
-  print "clusterName: $clusterName"
+  echo "clusterName: $clusterName"
   secret=$(kubectl get secret -n crossplane-system | awk '{print $1}' | grep "${xk8sUid}-ekscluster")
-  print "secretName: $secret"
+  echo "secretName: $secret"
   kubeconfig=$(kubectl get secret $secret -o=jsonpath='{.data.kubeconfig}' -n crossplane-system)
   echo "$kubeconfig" | base64 --decode > ~/.kube/$clusterName
 
 done
+
+
